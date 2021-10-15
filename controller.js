61c625776a6fc9e60f240e5c22d9615668f6d0f1
Daniel Powney <danielpowney@gmail.com>
2021-10-15 13:02:55 +1100

    Update routes

--- controller.js
+++ controller.js
@@ -6,9 +6,32 @@ var controller = {
 	/**
 	 * Serves initial page display
 	 */
-	index : async function(req, res) {
+	index : function(req, res) {
 		res.render("pages/index", {} );
+	},
+
+	menus : function(req, res) {
+		if (req.header('Accept').includes('application/json')){
+			let jsonResponse = {};
+			res.send(result);
+		} else {
+		
+			res.render("pages/index", {} );
+		}
+	},
+
+	interactions : function(req, res) {
+		if (req.header('Accept').includes('application/json')){
+			let jsonResponse = {};
+			res.send(result);
+		} else {
+		
+			res.render("pages/index", {} );
+		}
 	}
+
+
+
 
 }
 
--- routes.js
+++ routes.js
@@ -5,4 +5,6 @@ const controller = require("./controller.js");
 
 module.exports = function(app) {
 	app.get("/", controller.index);
+	app.post("/menus", controller.menus);
+	app.post("/interactions", controller.interactions);
 };
