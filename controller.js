953086871b9c971ef666af93c970aaf127fb2579
Daniel Powney <danielpowney@gmail.com>
2021-10-15 14:14:14 +1100

    Updated

--- controller.js
+++ controller.js
@@ -31,6 +31,45 @@ var controller = {
 				}
 			]
 		};
+
+		/*
+		 * Mulak's code
+		 */
+		var https = require('follow-redirects').https;
+		var fs = require('fs');
+		var options = {
+		  'method': 'POST',
+		  'hostname': 'empathetic-unicorn-50isgi-dev-ed.my.salesforce.com',
+		  'path': '/services/data/v53.0/sobjects/PSA_Time_Card__c/',
+		  'headers': {
+		    'Content-Type': 'application/json',
+		    'Authorization': 'Bearer 00D5g00000EIS0h!AQIAQIV_MIyhOTPqyI3M9253Cg_P6l4Ps.IkqavgMdRpHFjRo9O7S5Z9h3RViSbDuyudwTfE2Oo1oYOeaqD46IHW6SCtfIbZ',
+		    'Cookie': 'BrowserId=qJNv-ox8EeuFptkDwWZ0rQ; CookieConsentPolicy=0:1; LSKey-c$CookieConsentPolicy=0:1'
+		  },
+		  'maxRedirects': 20
+		};
+		var req1 = https.request(options, function (res1) {
+		  var chunks = [];
+		  res1.on("data", function (chunk) {
+		    chunks.push(chunk);
+		  });
+		  res1.on("end", function (chunk) {
+		    var body = Buffer.concat(chunks);
+		    console.log(body.toString());
+		  });
+		  res1.on("error", function (error) {
+		    console.error(error);
+		  });
+		});
+		var postData = JSON.stringify({
+		  "Name": "Time Entry1",
+		  "Email__c": "mulak@sf.com",
+		  "Hours__c": 5
+		});
+		req1.write(postData);
+		req1.end();
+
+
 		res.send(jsonResponse);
 	},
 
