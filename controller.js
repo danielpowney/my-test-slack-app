56ff9992ecad44969253e4723f0f231aae6a84cd
Daniel Powney <danielpowney@gmail.com>
2021-10-15 13:51:28 +1100

    Update controller.js

--- controller.js
+++ controller.js
@@ -10,32 +10,28 @@ var controller = {
 		res.render("pages/index", {} );
 	},
 
-	menus : function(req, res) {
-		//if (req.header('Accept').includes('application/json')){
-			let jsonResponse = {
-			    "blocks": [
-					{
-						"type": "section",
+	timecard : function(req, res) {
+		let jsonResponse = {
+		    "blocks": [
+				{
+					"type": "section",
+					"text": {
+						"type": "mrkdwn",
+						"text": "<https://org62.lightning.force.com/one/one.app#eyJjb21wb25lbnREZWYiOiJvbmU6YWxvaGFQYWdlIiwiYXR0cmlidXRlcyI6eyJhZGRyZXNzIjoiaHR0cHM6Ly9vcmc2Mi0tcHNlLm5hMTI4LnZpc3VhbC5mb3JjZS5jb20vYXBleC9QU0FUaW1lY2FyZEVudHJ5In0sInN0YXRlIjp7fX0%3D|Time Entry> :timeclock::partyparrot:"
+					},
+					"accessory": {
+						"type": "button",
 						"text": {
-							"type": "mrkdwn",
-							"text": "<https://org62.lightning.force.com/one/one.app#eyJjb21wb25lbnREZWYiOiJvbmU6YWxvaGFQYWdlIiwiYXR0cmlidXRlcyI6eyJhZGRyZXNzIjoiaHR0cHM6Ly9vcmc2Mi0tcHNlLm5hMTI4LnZpc3VhbC5mb3JjZS5jb20vYXBleC9QU0FUaW1lY2FyZEVudHJ5In0sInN0YXRlIjp7fX0%3D|Time Entry> :timeclock::partyparrot:"
+							"type": "plain_text",
+							"text": "Copy & Submit From Previous Week",
+							"emoji": true
 						},
-						"accessory": {
-							"type": "button",
-							"text": {
-								"type": "plain_text",
-								"text": "Copy & Submit From Previous Week",
-								"emoji": true
-							},
-							"value": "view_alternate_1"
-						}
+						"value": "view_alternate_1"
 					}
-				]
-			};
-			res.send(jsonResponse);
-		//} else {
-		//	res.render("pages/index", {} );
-		//}
+				}
+			]
+		};
+		res.send(jsonResponse);
 	},
 
 	interactions : function(req, res) {
